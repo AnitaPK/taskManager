50c0dc0209020db43030679eb0cc80d1ae4f47a3
AnitaPK <anita.khamitkar@gmail.com>
2023-07-05 12:40:15 +0530

    added comment to app.js file

--- taskManager/app.js
+++ taskManager/app.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
+// for testing purposes
 var testRoutes = require('./routes/crud');
 
 // Import my test routes into the path '/test'
 app.use('/v1', testRoutes);
 
-
+// to show on port no 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  //End of File
+  
 })
